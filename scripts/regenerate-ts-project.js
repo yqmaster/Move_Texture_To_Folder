43fcc6f6109f9dfe4c23b94c10ed658fc5d006c0
zhaoye <zhaoye@kingsoft.com>
2023-01-16 10:07:15 +0800

    feat: 提交部分脚本修改

--- scripts/regenerate-ts-project.js
+++ scripts/regenerate-ts-project.js
@@ -8,14 +8,14 @@ log(`switch to project root: ${projectRoot}`);
 process.chdir(projectRoot);
 
 log("Install global node modules...");
-exec("npm install typescript yarn protobufjs-cli rimraf@3.0.2 -g");
+exec("npm install typescript yarn rimraf -g");
 
 log("Cleanup project dist...");
 
-exec("rimraf -rf packages/*/dist packages/*/tsconfig.tsbuildinfo");
+exec("rimraf packages/dist packages/tsconfig.tsbuildinfo");
 
-function exec(command, cwd) {
-    child_process.execSync(command, { cwd: cwd, stdio: "inherit" });
+function exec(command) {
+    child_process.execSync(command);
 }
 
 function log(...args) {
